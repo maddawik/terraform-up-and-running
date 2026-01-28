a14400b13551c62e0c0213c8cb64659400495285
maddawik <edwardreillycomputers@gmail.com>
2026-01-28 11:36:42 -0500

    feat: get vpc and subnets as data source to use in ASG

--- main.tf
+++ main.tf
@@ -6,6 +6,17 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
@@ -30,6 +41,7 @@ EOF
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
   min_size = 2
   max_size = 10
