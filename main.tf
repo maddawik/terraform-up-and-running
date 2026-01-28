45e13b0d002e933251751a7d417bfe13bf93514a
maddawik <edwardreillycomputers@gmail.com>
2026-01-28 11:55:59 -0500

    feat: add a load-balancer and listener rule

--- main.tf
+++ main.tf
@@ -29,8 +29,32 @@ data "aws_subnets" "default" {
   }
 }
 
+### Appilcation Load Balancer
 
-### Launch Config & ASG
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+
+### Launch Config & Auto-scaling group
 
 resource "aws_launch_configuration" "example" {
   instance_type   = "t2.micro"
