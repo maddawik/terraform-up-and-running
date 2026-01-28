2fa3bc27b1826ba021086ecb8f8fcb494865e918
maddawik <edwardreillycomputers@gmail.com>
2026-01-28 13:13:30 -0500

    feat: add security group to alb

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_subnets" "default" {
   }
 }
 
-### Appilcation Load Balancer
+### Appilcation Load Balancer + Security Group
 
 resource "aws_lb" "example" {
   name               = "terraform-asg-example"
@@ -50,6 +50,26 @@ resource "aws_lb_listener" "example" {
       message_body = "404: page not found"
       status_code  = 404
     }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow inbound HTTP requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
