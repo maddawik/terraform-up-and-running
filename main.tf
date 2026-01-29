c1e84723d64a999b87f689c5dfbfe9da0d262137
maddawik <edwardreillycomputers@gmail.com>
2026-01-28 21:08:54 -0500

    feat: use alb dns for output

--- main.tf
+++ main.tf
@@ -153,7 +153,7 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
   description = "The public IP address of the web server"
 }
