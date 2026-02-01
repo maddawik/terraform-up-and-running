d2dcbaecb6dbebe0760347cb1084bca91fb858fb
maddawik <edwardreillycomputers@gmail.com>
2026-02-01 17:24:08 -0500

    feat(ch-3): add s3 bucket for shared state

---
+++ ch-3/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Statefile
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform_up_and_running"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
