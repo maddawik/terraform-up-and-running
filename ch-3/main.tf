8bc5a9729b2a56a1fba2a6efcab2192586e602eb
maddawik <edwardreillycomputers@gmail.com>
2026-02-02 01:04:53 -0500

    fix: dynamodb_table -> use_lockfile
    `dynamodb_table` is deprected in favor of `use_lockfile` which stores the
    lockfile in S3 as well, requiring 1 less AWS service.

--- ch-3/main.tf
+++ ch-3/main.tf
@@ -5,10 +5,11 @@ terraform {
     key    = "global/s3/terraform.tfstate"
     region = "us-east-2"
 
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
+    use_lockfile = true
+    encrypt      = true
   }
 }
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -39,26 +40,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   }
 }
 
-# Locks
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 # Outputs
 
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.terraform_state.arn
   description = "The ARN of the S3 bucket."
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table."
 }
