b1b9afa19c319400594e6e2bb5717b69f7c8c8df
maddawik <edwardreillycomputers@gmail.com>
2026-02-01 23:10:16 -0500

    feat: add dynamo db table for locking
    We also need a unique name for the S3 bucket

--- ch-3/main.tf
+++ ch-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # Statefile
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform_up_and_running"
+  bucket = "terraform-up-and-running-ch-307091e9f"
 
   lifecycle {
     prevent_destroy = true
@@ -25,5 +25,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+# Locks
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
