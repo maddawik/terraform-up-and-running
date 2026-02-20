4210cddd76c7c14f15f616383d6a4421e4eed79f
maddawik <edwardreillycomputers@gmail.com>
2026-02-19 22:15:36 -0500

    feat: add scheduled auto-scaling using module outputs

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -1,4 +1,9 @@
 output "alb_dns_name" {
   value       = aws_lb.example.dns_name
-  description = "The public IP address of the web server"
+  description = "The domain name of the load balancer"
+}
+
+output "asg_name" {
+  value       = aws_autoscaling_group.example.name
+  description = "The name of the Auto Scaling Group"
 }
