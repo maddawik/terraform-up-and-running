7e8934ceccb53b66d9572918df00f7cc92a4c41f
maddawik <edwardreillycomputers@gmail.com>
2026-02-20 14:26:11 -0500

    feat: add iam module, use count.index for unique names - we can do better...

---
+++ global/iam/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.14.3"
+  backend "s3" {
+    bucket = "terraform-up-and-running-ch-307091e9f"
+    key    = "global/iam/terraform.tfstate"
+    region = "us-east-2"
+
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_iam_user" "example" {
+  count = 3
+  name  = "neo.${count.index}"
+}
