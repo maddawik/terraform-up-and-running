6d409db0abe42f0e73d41109b77d64044e6009ca
maddawik <edwardreillycomputers@gmail.com>
2026-02-19 22:19:44 -0500

    fix: use path reference expression for module root to fix user-data.sh

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -116,7 +116,7 @@ resource "aws_launch_template" "example" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = base64encode(templatefile("user-data.sh", {
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
